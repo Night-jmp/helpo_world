b74bb4daaaab31d6a7b8ca2b293895165d1797fc
Blake Janes <blakeinus@gmail.com>
2022-07-16 17:03:49 -0400

    Fixed Disassemly to not use pwntools

--- disassembler.py
+++ disassembler.py
@@ -1,16 +1,6 @@
-#from capstone import *
-from pwn import *
-#def disasm():
-
-#    CODE = open("hello", "rb").read()
-#    md = Cs(CS_ARCH_X86, CS_MODE_64)
-#    for i in md.disasm(CODE, 0): 
-#        print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
-
-def disasm():
-    e = ELF("hello")
-    addr = e.symbols['main']
-
-
-if __name__ == "__main__":
-    disasm()
+import rzpipe
+def disasm(filename):
+    rz = rzpipe.open("hello")
+    rz.cmd('aa')
+    rz.cmd('s sym.main')
+    print(rz.cmd('pdf'))
