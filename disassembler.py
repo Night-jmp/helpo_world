345fb8e15cfa1ac71d3b9d05bb709b2993a31a90
= <jconnolly80911@gmail.com>
2022-07-16 16:52:29 -0400

    pwntools pls

--- disassembler.py
+++ disassembler.py
@@ -1,8 +1,16 @@
-from capstone import *
+#from capstone import *
+from pwn import *
+#def disasm():
+
+#    CODE = open("hello", "rb").read()
+#    md = Cs(CS_ARCH_X86, CS_MODE_64)
+#    for i in md.disasm(CODE, 0): 
+#        print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
 
 def disasm():
+    e = ELF("hello")
+    addr = e.symbols['main']
 
-    CODE = open("hello", "rb").read()
-    md = Cs(CS_ARCH_X86, CS_MODE_64)
-    for i in md.disasm(CODE, 0):
-        print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
+
+if __name__ == "__main__":
+    disasm()
