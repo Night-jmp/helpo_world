eb7edc0792e8de2294d15551f64f7f7cbf5eda79
= <jconnolly80911@gmail.com>
2022-07-16 16:38:16 -0400

    Forgot to push this w00ps

--- disassembler.py
+++ disassembler.py
@@ -1,8 +1,8 @@
 from capstone import *
 
-def disasm(filename):
+def disasm():
 
-    CODE = open(filename, "rb").read()
+    CODE = open("hello", "rb").read()
     md = Cs(CS_ARCH_X86, CS_MODE_64)
     for i in md.disasm(CODE, 0):
         print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
